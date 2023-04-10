728d5941911ae759751a36db37dba76d706ec253
vaDEAD <t0rm0z.vad.tru@gmail.com>
2023-04-10 14:09:02 +0300

    Доработка дз

--- src/test/java/ru/netology/LoginTest.java
+++ src/test/java/ru/netology/LoginTest.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
-public class loginTest {
+public class LoginTest {
 
     @BeforeEach
     void setup() {
